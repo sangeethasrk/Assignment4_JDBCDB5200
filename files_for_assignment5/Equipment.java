827b381406a1f13c960fe60144e816c6edb9ff5e
geethsrk <sivaramakrishnan.s@husky.neu.edu>
2015-04-14 21:17:30 -0400

    Revert "Revert "Initial Commit for Assignment 5""
    This reverts commit b41d726149f253337bd0a17505380d2d343a706d.

---
+++ files_for_assignment5/Equipment.java
@@ -0,0 +1,74 @@
+package entity;
+import javax.persistence.*;
+import java.io.Serializable;
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+@Entity
+@NamedQuery(name="Equipment.findAll", query="SELECT eq FROM Equipment eq")
+public class Equipment implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String brand;
+	private String description;
+	private String name;
+	private double price;
+
+	@ManyToOne
+	@JoinColumn(name="towerId")
+	@JsonIgnore
+	private Tower towerobj;
+
+	public Equipment() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getBrand() {
+		return this.brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Tower getTower() {
+		return this.towerobj;
+	}
+
+	public void setTower(Tower towerobj) {
+		this.towerobj = towerobj;
+	}
+}
---
+++ files_for_assignment5/Site.java
@@ -0,0 +1,81 @@
+package entity;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@NamedQuery(name="findAllSites", query="SELECT s FROM Site s")
+public class Site implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private double latitude;
+	private double longitude;
+	private String name;
+
+	@OneToMany(mappedBy="site")
+	private List<Tower> towers;
+
+	public Site() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getLatitude() {
+		return this.latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return this.longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Tower> getTowers() {
+		return this.towers;
+	}
+
+	public void setTowers(List<Tower> towers) {
+		this.towers = towers;
+	}
+
+	public Tower addTower(Tower tower) {
+		getTowers().add(tower);
+		tower.setSite(this);
+		return tower;
+	}
+
+	public Tower removeTower(Tower tower) {
+		getTowers().remove(tower);
+		tower.setSite(null);
+		return tower;
+	}
+
+	@Override
+	public String toString() {
+		return "Site [id=" + id + ", latitude=" + latitude + ", longitude="
+				+ longitude + ", name=" + name + ", towers=" + towers + "]";
+	}
+}
