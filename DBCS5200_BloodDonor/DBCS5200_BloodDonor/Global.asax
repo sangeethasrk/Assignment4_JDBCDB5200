75a68387cedcaa9465d3f78a2cf20f86bbc123f0
sangeethasrk <sangeetha.svrmkrshnn@gmail.com>
2015-04-25 00:01:57 -0400

    Initial Commit for CS5200 Project
    Initial commit of the database management systems project

---
+++ DBCS5200_BloodDonor/DBCS5200_BloodDonor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DBCS5200_BloodDonor.MvcApplication" Language="C#" %>
---
+++ DBCS5200_BloodDonor/DBCS5200_BloodDonor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DBCS5200_BloodDonor.Startup))]
+namespace DBCS5200_BloodDonor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
